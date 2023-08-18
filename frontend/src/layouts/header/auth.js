251356d9836a2623b72649bc22668a5d84e9b4b8
Admin <admin@Admins-MBP.attlocal.net>
2023-08-18 15:38:34 -0700

    test

--- frontend/src/layouts/header/auth.js
+++ frontend/src/layouts/header/auth.js
@@ -89,8 +89,7 @@ const AuthHeader = () => {
                                 menu={{
                                     items: items,
                                     onClick: menuChangeHandle,
-                                }}
-                            >
+                                }}>
                                 <Avatar
                                     size="large"
                                     style={{
@@ -98,8 +97,7 @@ const AuthHeader = () => {
                                         cursor: "pointer",
                                         background: "#8f3dce"
                                     }}
-                                    src={auth?.user?.photo}
-                                >
+                                    src={auth?.user?.photo}>
                                     {auth?.user?.firstname?.[0]}
                                 </Avatar>
                             </Dropdown>
