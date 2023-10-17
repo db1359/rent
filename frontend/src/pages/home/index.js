3015796c8e2028399b919cbf0cc73271bda31682
Ava Paris <dbaran@gmail.com>
2023-10-17 10:39:58 -0700

    test remove

--- frontend/src/pages/home/index.js
+++ frontend/src/pages/home/index.js
@@ -27,7 +27,7 @@ const HomePage = () => {
                 Every 60 seconds a child and parent are separated by Family Courts for profit.
             </HomeSlogan> */}
             <HomeSubtitle>
-                <h1>Exposing Family Corruption & Fraud - Test</h1>
+                <h1>Exposing Family Corruption & Fraud</h1>
             </HomeSubtitle>
             <HomeSlogan>
                 Every 60 seconds a child & parent are separated for profit.
